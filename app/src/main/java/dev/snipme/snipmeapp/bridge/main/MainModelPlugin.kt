742a5aae9a58617a5d708aa255a65df8d82dd7fd
tmaxxdd <tmax0135@gmail.com>
2025-02-18 18:31:59 +0100

    Corrected types

--- app/src/main/java/dev/snipme/snipmeapp/bridge/main/MainModelPlugin.kt
+++ app/src/main/java/dev/snipme/snipmeapp/bridge/main/MainModelPlugin.kt
@@ -6,22 +6,29 @@ import dev.snipme.snipmeapp.bridge.ModelPlugin
 import dev.snipme.snipmeapp.bridge.toModelData
 import dev.snipme.snipmeapp.domain.snippets.Snippet
 import dev.snipme.snipmeapp.domain.snippets.SnippetFilters
+import dev.snipme.snipmeapp.channel.MainModelBridge as ChannelMainModelBridge
+import dev.snipme.snipmeapp.channel.MainModelStateData as ChannelMainModelStateData
+import dev.snipme.snipmeapp.channel.MainModelEventData as ChannelMainModelEventData
+import dev.snipme.snipmeapp.channel.MainModelEvent as ChannelMainModelEvent
+import dev.snipme.snipmeapp.channel.ModelState as ChannelModelState
+import dev.snipme.snipmeapp.channel.SnippetFilter as ChannelSnippetFilter
 
-class MainModelPlugin : ModelPlugin<Bridge.MainModelBridge>(), Bridge.MainModelBridge {
+
+class MainModelPlugin : ModelPlugin<ChannelMainModelBridge>(), ChannelMainModelBridge {
     private val model: MainModel by inject()
     private var oldEvent: MainEvent? = null
     private var oldState: MainViewState? = null
 
     override fun onSetup(
         messenger: BinaryMessenger,
-        bridge: Bridge.MainModelBridge?
+        bridge: ChannelMainModelBridge?
     ) {
-        Bridge.MainModelBridge.setUp(messenger, bridge)
+        ChannelMainModelBridge.setUp(messenger, bridge)
     }
 
-    override fun getState(): Bridge.MainModelStateData = getState(model.state.value)
+    override fun getState(): ChannelMainModelStateData = getState(model.state.value)
 
-    override fun getEvent(): Bridge.MainModelEventData = getEvent(model.event.value)
+    override fun getEvent(): ChannelMainModelEventData = getEvent(model.event.value)
 
     override fun resetEvent() {
         model.event.value = Startup
@@ -43,53 +50,52 @@ class MainModelPlugin : ModelPlugin<Bridge.MainModelBridge>(), Bridge.MainModelB
         model.logOut()
     }
 
-    private fun getState(viewState: MainViewState): Bridge.MainModelStateData {
-        return Bridge.MainModelStateData().apply {
-            state = viewState.toModelState()
-            isLoading = viewState is Loading
-            data = (viewState as? Loaded)?.snippets?.toModelData()
-            filter = (viewState as? Loaded)?.filters?.toModelFilter()
-            oldHash = oldState?.hashCode()?.toLong()
-            newHash = viewState.hashCode().toLong()
-        }.also {
+    private fun getState(viewState: MainViewState): ChannelMainModelStateData {
+        return ChannelMainModelStateData(
+            state = viewState.toModelState(),
+            isLoading = viewState is Loading,
+            data = (viewState as? Loaded)?.snippets?.toModelData(),
+            filter = (viewState as? Loaded)?.filters?.toModelFilter(),
+            oldHash = oldState?.hashCode()?.toLong(),
+            newHash = viewState.hashCode().toLong(),
+        ).also {
             oldState = viewState
         }
     }
 
-    private fun getEvent(viewEvent: MainEvent): Bridge.MainModelEventData {
-        return Bridge.MainModelEventData().apply {
-            event = viewEvent.toModelEvent()
-            message = (viewEvent as? Alert)?.message
-            oldHash = oldEvent?.hashCode()?.toLong()
-            newHash = viewEvent.hashCode().toLong()
-        }.also {
+    private fun getEvent(viewEvent: MainEvent): ChannelMainModelEventData {
+        return ChannelMainModelEventData(
+            event = viewEvent.toModelEvent(),
+            message = (viewEvent as? Alert)?.message,
+            oldHash = oldEvent?.hashCode()?.toLong(),
+            newHash = viewEvent.hashCode().toLong(),
+        ).also {
             oldEvent = viewEvent
         }
     }
 
     private fun MainEvent.toModelEvent() =
         when (this) {
-            is Alert -> Bridge.MainModelEvent.ALERT
-            is Logout -> Bridge.MainModelEvent.LOGOUT
-            else -> Bridge.MainModelEvent.NONE
+            is Alert -> ChannelMainModelEvent.ALERT
+            is Logout -> ChannelMainModelEvent.LOGOUT
+            else -> ChannelMainModelEvent.NONE
         }
 
     private fun MainViewState.toModelState() =
         when (this) {
-            Loading -> Bridge.ModelState.LOADING
-            is Loaded -> Bridge.ModelState.LOADED
-            is Error -> Bridge.ModelState.ERROR
+            Loading -> ChannelModelState.LOADING
+            is Loaded -> ChannelModelState.LOADED
+            is Error -> ChannelModelState.ERROR
         }
 
     private fun List<Snippet>.toModelData() = map { it.toModelData() }
 
-    private fun SnippetFilters.toModelFilter(): Bridge.SnippetFilter {
-        val it = this
-        return Bridge.SnippetFilter().apply {
-            languages = it.languages
-            selectedLanguages = it.selectedLanguages
-            scopes = it.scopes
-            selectedScope = it.selectedScope
-        }
+    private fun SnippetFilters.toModelFilter(): ChannelSnippetFilter {
+        return ChannelSnippetFilter(
+            languages = languages,
+            selectedLanguages = selectedLanguages,
+            scopes = scopes,
+            selectedScope = selectedScope,
+        )
     }
 }
