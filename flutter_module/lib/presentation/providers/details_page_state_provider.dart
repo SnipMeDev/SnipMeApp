5e15ddfe9bbfd01b498522fedf01d96ee045a4ec
tmaxxdd <tmax0135@gmail.com>
2025-02-27 10:33:03 +0100

    Corrected details flow

--- flutter_module/lib/presentation/providers/details_page_state_provider.dart
+++ flutter_module/lib/presentation/providers/details_page_state_provider.dart
@@ -1,25 +1,24 @@
 import 'package:flutter_module/presentation/providers/channel_stream_state_notifier.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../bridge/generated.g.dart';
 import '../../generated/data_model.g.dart';
 import 'channel_model_provider.dart';
 
 final detailsPageStateProvider = StateNotifierProvider<
-    ChannelStreamStateNotifier<DetailModelStateData, ChannelDetailsModel>,
-    DetailModelStateData>(
-      (ref) => ChannelStreamStateNotifier(
-    initialState: DetailModelStateData(),
+    ChannelStreamStateNotifier<DetailsModelStateData, ChannelDetailsModel>,
+    DetailsModelStateData>(
+  (ref) => ChannelStreamStateNotifier(
+    initialState: DetailsModelStateData(),
     model: ref.read(detailsChannelModel),
     dataStream: channelState(),
   ),
 );
 
 final detailsPageEventProvider = StateNotifierProvider<
-    ChannelStreamStateNotifier<DetailModelEventData, ChannelDetailsModel>,
-    DetailModelEventData>(
-      (ref) => ChannelStreamStateNotifier(
-    initialState: DetailModelEventData(),
+    ChannelStreamStateNotifier<DetailsModelEventData, ChannelDetailsModel>,
+    DetailsModelEventData>(
+  (ref) => ChannelStreamStateNotifier(
+    initialState: DetailsModelEventData(),
     model: ref.read(detailsChannelModel),
     dataStream: channelEvent(),
   ),
